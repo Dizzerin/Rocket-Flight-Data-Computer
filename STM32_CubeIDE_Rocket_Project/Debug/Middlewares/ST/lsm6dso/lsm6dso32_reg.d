a467f52bbe3820c0e2cc32d1629a339d2a6780c5
Caleb Nelson <dizzerin@gmail.com>
2026-04-24 00:01:45 -0700

    Removed Debug folder from git cause it shouldn't be under version control

--- STM32_CubeIDE_Rocket_Project/Debug/Middlewares/ST/lsm6dso/lsm6dso32_reg.d
+++
@@ -1,4 +0,0 @@
-Middlewares/ST/lsm6dso/lsm6dso32_reg.o: \
- ../Middlewares/ST/lsm6dso/lsm6dso32_reg.c \
- ../Middlewares/ST/lsm6dso/lsm6dso32_reg.h
-../Middlewares/ST/lsm6dso/lsm6dso32_reg.h:
--- STM32_CubeIDE_Rocket_Project/Debug/Middlewares/Third_Party/BOSCH_BME/bme68x.d
+++
@@ -1,6 +0,0 @@
-Middlewares/Third_Party/BOSCH_BME/bme68x.o: \
- ../Middlewares/Third_Party/BOSCH_BME/bme68x.c \
- ../Middlewares/Third_Party/BOSCH_BME/bme68x.h \
- ../Middlewares/Third_Party/BOSCH_BME/bme68x_defs.h
-../Middlewares/Third_Party/BOSCH_BME/bme68x.h:
-../Middlewares/Third_Party/BOSCH_BME/bme68x_defs.h:
